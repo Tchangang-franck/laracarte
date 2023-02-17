5f41bc4744c87f1cde43776196803c16f3e02203
Tchangang-franck <clautialidris@gmail.com>
2023-02-17 09:35:58 +0100

    add conf('app.name')

--- resources/views/layouts/partials/_footer.blade.php
+++ resources/views/layouts/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="text-center">
-    <p>&copy;  {{ date('y-m-d') }} &middot; laracarte by <a href="https://twiter.com/franck">@franck</a> </p>
+    <p>&copy;  {{ date('y-m-d') }} &middot; {{ config('app.name') }} by <a href="https://twiter.com/franck">@franck</a> </p>
     -A <a href="https://laramaps.com">- A laramap clone app.</a>
      <p> <strong>this app has been  build for learning purpose,</strong></p>
 </footer>
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts/default',['title'=>'Abort'])
     @section('content')
         <div class="container">
-            <h2> What is Laracarte?</h2>
-            <p>Laracarte is a clone of <a href="http://laramap.com" target="_blank">Laramap.com</a></p>
+            <h2> What is {{ config('app.name') }}?</h2>
+            <p>{{ config('app.name') }} is a clone of <a href="http://laramap.com" target="_blank">Laramap.com</a></p>
             <div class="row">
                 <div class="col-md-l">
                     <p class="alert alert-warning"><strong><i class="fa fa-exclamation-triangle" aria-hidden="true"></i>this app has been buil by<a href="http://twitter.com/franck"> @franck</a> for learning pupose</strong></p>
                 </div> 
-                <p>feel free to help to improve the<a href="https://github.com/Tchangang-franck/laracarte"> source code</a></p>
+                <p>feel free to help to improve the<a href="https://github.com/Tchangang-franck/{{ config('app.name') }}"> source code</a></p>
             </div>
             <hr class="divider">
             <h2>What is laramap</h2>
-            <p>Laramap is the  wesbsite by which Laracarte was inspire :).</p>
+            <p>Laramap is the  wesbsite by which {{ config('app.name') }} was inspire :).</p>
             More infore <a href="http://laramap.com/p.about">here.</a>
             <hr class="divider">
-            <h2>Which tool ans services  are use in Laracarte ?</h2>
+            <h2>Which tool ans services  are use in {{ config('app.name') }}?</h2>
             <p>Basically it's build on laravel  & Bootstrap . But there's a bunch of services used for email and other action.</p>
             <ul>
                 <li>Laravel</li>

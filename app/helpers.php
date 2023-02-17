327bdb71338cf4de938326bcfe258b1e3b847898
Tchangang-franck <clautialidris@gmail.com>
2023-02-17 10:06:51 +0100

    page d'eereur customiser

--- app/helpers.php
+++ app/helpers.php
@@ -3,7 +3,7 @@ use Illuminate\Support\Facades\Route;
 
 if( ! function_exists('page_title')){
     function page_title($title){
-        $base_title='Laracarte List of artisan';
+        $base_title= config('app.name') .'List of artisan';
         if($title===''){
             return $base_title;
         }
